41c096ba1b959403edf6d2cd33105d5ebf201709
tanumodi <43467108+tanumodi@users.noreply.github.com>
2019-06-07 11:06:41 +0530

    Delete 2_2.js

--- 2_2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#btn").on("click", function() {
-    var input = $(this).val().toLowerCase();
-    $("#t1 tr").filter(function() {
-      $(this).hide($(this).text().toLowerCase().indexOf(input) > -1)
-    });
-  });
-});
